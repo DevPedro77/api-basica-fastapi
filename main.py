b149a9f1f1b68f70650e61ca89204257bca81da7
Pedro Castro <pcastro9876@gmail.com>
2025-09-26 15:29:03 -0300

    feat: Criando rota de listagem de cursos

--- main.py
+++ main.py
@@ -2,11 +2,22 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/")
-async def raiz():
-    return {"Servidor Rodando": "OK"}
+cursos_dict = {
+  1: {"titulo": "Curso de Python", "aulas": 10, "horas": 20},
+  2: {"titulo": "Curso de JavaScript", "aulas": 15, "horas": 25},
+  3: {"titulo": "Curso de Java", "aulas": 20, "horas": 30} 
+}
+
+@app.get("/cursos")
+async def listar_cursos():
+    return cursos_dict
+  
+@app.get("/cursos/{curso_id}")
+async def obter_curso(curso_id: int):
+    curso = cursos_dict[curso_id]
+    curso.update({"id": curso_id})
+    return curso
 
 if __name__ == "__main__":
     import uvicorn
-
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, log_level="info", reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
